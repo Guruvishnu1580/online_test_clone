a162bf42d6fd71944e222c2ea86c4d98bf8acf6e
[Guruvishnu ] <[guruvishnu.kajagar@candorworks.com]>
2025-12-15 13:46:33 +0530

    add css style to HRLayout

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Users ,User,ShieldQuestionMark ,BookOpenCheck,LayoutDashboard} from 'lucide-react'
+
 import '../../App.css'
 const Sidebar = () => {
   return (
     <nav className='sidebar'>
         <ul>
-            <li><Link to="/hr">Dashboard</Link></li>
-            <li><Link to="/hr/users">User Management</Link></li>
-            <li><Link to="/hr/candidates">Candidates</Link></li>
-            <li><Link to="/hr/questions">Questions</Link></li>
-            <li><Link to="/hr/results">
-            Results</Link></li>
+            <li><Link to="/hr"><LayoutDashboard /> Dashboard</Link></li>
+            <li><Link to="/hr/users"><Users/> User Management</Link></li>
+            <li><Link to="/hr/candidates"><User /> Candidates</Link></li>
+            <li><Link to="/hr/questions"><ShieldQuestionMark /> Questions</Link></li>
+            <li><Link to="/hr/results"><BookOpenCheck /> Results</Link></li>
         </ul>
     </nav>
   )
